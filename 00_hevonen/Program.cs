91e67a67a91377df9fc2561516efc2fa8be11ac1
laeroy <52239281+Laeroy@users.noreply.github.com>
2021-08-11 13:58:46 +0300

    Initial commit

---
+++ 00_hevonen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _00_hevonen
+{
+    class Program
+    {
+        public class Horse
+        {
+            public string name;
+            public int weight;
+
+            public Horse()
+            {
+                name = "default";
+                weight = 100;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Horse horse = new();
+            Console.WriteLine($"{horse.name} {horse.weight}");
+            horse.name = "Histamiini";
+            horse.weight = 89;
+            Console.WriteLine($"{horse.name} {horse.weight}");
+        }
+    }
+}
