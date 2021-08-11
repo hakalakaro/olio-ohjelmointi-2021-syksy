1147d8e04718c9f87a8819f595333b9a00893e1d
Lukanja <tekotaide@hotmail.com>
2021-08-11 14:03:06 +0300

    Hevonen palautettu.

---
+++ 00_hevonen/00_hevonen_koodi/00_hevonen_koodi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _00_hevonen_koodi
+{ 
+    class Hevonen
+    {
+        public string nimi;
+        public string paino;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Hevonen olio = new Hevonen();
+            Console.WriteLine(olio.nimi);
+            Console.WriteLine(olio.paino);
+            olio.nimi = "Histamiini";
+            olio.paino = "89";
+            Console.WriteLine(olio.nimi);
+            Console.WriteLine(olio.paino);
+
+        }
+    }
+}
